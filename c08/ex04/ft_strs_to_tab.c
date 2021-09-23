5261b9cbfbf9dfa11145933453182f566bf67793
Fatmeat-RP <82979497+Fatmeat-RP@users.noreply.github.com>
2021-09-23 04:59:09 +0200

    incroyable

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -6,13 +6,59 @@
 /*   By: acarle-m <acarle-m@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 01:33:49 by acarle-m          #+#    #+#             */
-/*   Updated: 2021/09/21 12:56:28 by acarle-m         ###   ########lyon.fr   */
+/*   Updated: 2021/09/23 04:36:22 by acarle-m         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*p;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	p = malloc((i + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		p[i] = src[i];
+		i++;
+	}
+	p[i] = 0x00;
+	return (p);
+}
+
 struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	char
+	struct s_stock_str	*par;
+	int					n;
 
-	while()
+	par = malloc((ac + 1) * sizeof(struct s_stock_str));
+	if (!par)
+		return (NULL);
+	n = 0;
+	while (n < ac)
+	{
+		par[n].size = ft_strlen(av[n]);
+		par[n].str = av[n];
+		par[n].copy = ft_strdup(av[n]);
+		n++;
+	}
+	return (par);
 }
---
+++ c08/ex05/ft_show_tab.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarle-m <acarle-m@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 03:30:54 by acarle-m          #+#    #+#             */
+/*   Updated: 2021/09/23 04:57:41 by acarle-m         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	 write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_negnbr(unsigned int nb)
+{	
+	if (nb > 9)
+	{
+		ft_negnbr(nb / 10);
+		nb %= 10;
+	}
+	ft_putchar(nb + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	int	n;
+
+	n = nb;
+	if (nb == 0)
+	{
+		ft_putchar('0');
+	}
+	if (nb <= -1)
+	{
+		ft_putchar('-');
+		ft_negnbr(nb * -1);
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		n %= 10;
+	}
+	ft_putchar(n + '0');
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	n;
+
+	n = 0;
+	while (par[n].str)
+	{
+		ft_putstr(par[n].str);
+		ft_putchar('\n');
+		ft_putnbr(par[n].size);
+		ft_putchar('\n');
+		ft_putstr(par[n].copy);
+		ft_putchar('\n');
+		n++;
+	}
+}
